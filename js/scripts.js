4448232479b7b6a572cb23d500ed9f8ca3427949
gracie lundell and nathan otto <student@epicodus.com>
2016-01-28 09:46:48 -0800

    Begin adding UI

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@
 // score;
 
 function HangMan(){
-  this.array = ["cat"];
+  this.array = ["tree"];
   this.word = this.randomWord();
-  this.guessedLetters = []
+  this.guessedLetters = [];
   this.score = 5;
+  this.userGuess = this.checkLetter;
 };
 
 HangMan.prototype.randomWord = function() {
@@ -28,18 +29,18 @@ HangMan.prototype.splitWord = function(word) {
 
 // check guessed letters against letters in word
 // if letter not in word, add letter to guessed letter, subtract 1 from score (when score === 0 you lose)
-// if letter is in word, replace "_" with guessed letter
-HangMan.prototype.guessLetter = function(word) {
-  var guessLetter = function {
-    for (i = 0; i <= word.length; i ++) {
-      if (word[i]) !== guessLetter)
-        this.score -= 1
-        continue
-    }
-
-    if (guessLetter !== word)
-
-
-    for (guessLetter !== )
-  }
-}
+// else letter is in word, replace "_" with guessed letter
+HangMan.prototype.checkLetter = function(guessLetter) {
+  var checkLetter = this.userGuess;
+  for (i = 0; i <= this.word.length; i ++) {
+    if (this.word.charAt(i) !== guessLetter) {
+      guessLetter.push(this.guessedLetters)
+      this.score -= 1
+    } else {
+      addBlanks.charAt(i).replace(guessLetter);
+      guessLetter.push(guessedLetters)
+      this.score = score;
+    };
+    return addBlanks
+  };
+};
--- spec/specs.js
+++ spec/specs.js
@@ -1,27 +1,22 @@
 describe("HangMan", function() {
-  it("will create a new HangMan", function() {
-    var testHangMan = new HangMan(["cat"]);
-    expect(testHangMan.array).to.eql(["cat"]);
-  });
-
   it("will select a word from the array", function() {
-    var testHangMan = new HangMan(["cat"]);
-    expect(testHangMan.randomWord(["cat"])).to.equal("cat");
+    var testHangMan = new HangMan(["tree"]);
+    expect(testHangMan.randomWord(["tree"])).to.equal("tree");
   });
 
   it("will replace letters in the word with '_'", function() {
-    var testHangMan = new HangMan(["cat"]);
-    expect(testHangMan.addBlanks(["cat"])).to.equal("_ _ _ ");
+    var testHangMan = new HangMan(["tree"]);
+    expect(testHangMan.addBlanks(["tree"])).to.equal("_ _ _ _ ");
   });
 
   it("will split letters in chosenWord", function() {
-    var testHangMan = new HangMan(["cat"]);
-    expect(testHangMan.splitWord(["cat"])).to.eql([ 'c', 'a', 't' ]);
+    var testHangMan = new HangMan(["tree"]);
+    expect(testHangMan.splitWord(["tree"])).to.eql([ 't', 'r', 'e', 'e' ]);
   });
 
   it("will check if guessed letter is in word", function() {
-    var testHangMan = new HangMan("cat");
-    expect(testHangMan.guessLetter("b")).to.equal(false);
+    var testHangMan = new HangMan("tree");
+    expect(testHangMan.checkLetter(score)).to.equal(4);
   });
 
 });
